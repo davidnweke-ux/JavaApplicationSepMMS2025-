beb18d8cf98b42ed6ab94833d14b839309be95f9
David Nweke <davidnwekealex@example.com>
2026-05-07 16:07:38 +0100

    day three

---
+++ EscapeCharater.java
@@ -0,0 +1,10 @@
+public class EscapeCharater{
+	public static void main(String[] arges) {
+		System.out.print("When you need to use the escape characters \n");
+		System.out.print("Use it when you want to  manaipulate your cursor");
+		System.out.print("Use \\n when you want a newline\n");
+		System.out.print("Who is the father of java?. The father of java is \"James Gosling\"");
+		System.out.print("Use this when you want a tab or indentation \n");
+		System.out.print("We are using carriage return here");
+	}
+}
